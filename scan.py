bd886342963bffa7df8b777fca62428011d06b37
Hawax <99415145+hawaxgit@users.noreply.github.com>
2023-05-16 19:40:46 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -9,32 +9,28 @@ SCRIPT_NAME = "Portscanner"
 VERSION = "1.0"
 WEBSITE = "www.hawax.de"
 
-# Signalverarbeitungsfunktion
 def signal_handler(sig, frame):
-    print('Portscan wurde beendet')
+    print('Port scan has ended')
     sys.exit(0)
 
 def scan_port(ip, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            print(f"Port {port} offen auf {ip}")
-        sock.close()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(0.5)
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print(f"Port {port} open up {ip}")
     except KeyboardInterrupt:
         sys.exit(0)
-    except:
+    except Exception as e:
         pass
 
 def scan(args):
     signal.signal(signal.SIGINT, signal_handler)
     with ThreadPoolExecutor(max_workers=200) as executor:
-        futures = []
-        for port in range(args.start_port, args.end_port):
-            futures.append(executor.submit(scan_port, args.ip, port))
+        futures = [executor.submit(scan_port, args.ip, port) for port in range(args.start_port, args.end_port)]
 
-        for future in as_completed(futures):
+        for _ in as_completed(futures):
             pass
 
     print("\n" + "-" * 60)
@@ -46,13 +42,13 @@ def scan(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Portscanner")
-    parser.add_argument("ip", help="IP-Adresse des Zielrechners")
+    parser.add_argument("ip", help="IP address of the target computer")
     parser.add_argument("--start-port", type=int, default=1, help="Startport")
     parser.add_argument("--end-port", type=int, default=65535, help="Endport")
     args = parser.parse_args()
 
     print("-" * 60)
-    print(f"Willkommen beim {SCRIPT_NAME} Version {VERSION}")
+    print(f"Welcome to {SCRIPT_NAME} Version {VERSION}")
     print(f"Autor: {AUTHOR}")
     print(f"Website: {WEBSITE}")
     print("-" * 60)
