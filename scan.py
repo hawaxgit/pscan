1bfef0d2e7cb44c9f05ddd899319dc8887894e81
Nithesh Balaji <nithesh.balaji@fundsindia.com>
2023-06-26 15:56:02 +0530

    Refactored code: added condition for "no open ports found" in range, improved handling of last element, and fixed typos

--- scan.py
+++ scan.py
@@ -8,18 +8,21 @@ AUTHOR = "Soroush Tavanaei at Hawax IT"
 SCRIPT_NAME = "Portscanner"
 VERSION = "1.0"
 WEBSITE = "www.hawax.de"
+open_ports_found = False
 
 def signal_handler(sig, frame):
     print('Port scan has ended')
     sys.exit(0)
 
 def scan_port(ip, port):
+    global open_ports_found
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.settimeout(0.5)
             result = sock.connect_ex((ip, port))
             if result == 0:
-                print(f"Port {port} open up {ip}")
+                print(f"Port {port} open on {ip}")
+                open_ports_found = True
     except KeyboardInterrupt:
         sys.exit(0)
     except Exception as e:
@@ -28,10 +31,13 @@ def scan_port(ip, port):
 def scan(args):
     signal.signal(signal.SIGINT, signal_handler)
     with ThreadPoolExecutor(max_workers=200) as executor:
-        futures = [executor.submit(scan_port, args.ip, port) for port in range(args.start_port, args.end_port)]
+        futures = [executor.submit(scan_port, args.ip, port) for port in range(args.start_port, args.end_port+1)]
 
         for _ in as_completed(futures):
             pass
+
+    if not open_ports_found:
+        print("\nNo open ports found in the given range")
 
     print("\n" + "-" * 60)
     print(f"{SCRIPT_NAME} Version {VERSION}")
@@ -49,7 +55,7 @@ if __name__ == "__main__":
 
     print("-" * 60)
     print(f"Welcome to {SCRIPT_NAME} Version {VERSION}")
-    print(f"Autor: {AUTHOR}")
+    print(f"Author: {AUTHOR}")
     print(f"Website: {WEBSITE}")
     print("-" * 60)
 
